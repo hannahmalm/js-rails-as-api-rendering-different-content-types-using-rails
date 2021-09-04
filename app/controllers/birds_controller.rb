219a1d42969eb444b693b182a19c5b2b55ea8529
Hannah Malm <hannahemalm616@gmail.com>
2021-09-04 18:46:58 -0400

    commit tree 2bc9186e67d686296e58850dca229953b7a3af1c

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,16 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    #render 'birds/index.html.erb'
+    #to render plain text from a Rails controller, you specify plain:, followed by the text you want to display
+    #render plain: "Hello #{@birds[3].name}"
+    #render json: 'Remember that JOSN is just object notation converted to string data, so strings work here '
+    #render json: { message: 'Hashes of data will get converted to JSON' }
+    #render json: ['As', 'well', 'as', 'arrays']
+    #we already have a collection of data, @birds
+    #no longer need instance variables
+    render json: birds
   end
 end
+
+#to confim that plain is working, we need a frontend to send a request to our Rails app - HTML file
